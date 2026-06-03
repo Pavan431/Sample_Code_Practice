da9b7110dcca47706074e0ae4a4293e12a3e90a3
sample practice <pav.kumar07@gmail.com>
2026-06-03 12:43:35 +0530

    Revert "Add prog3, prog4 and prog5"
    This reverts commit 570681a954e1b4f744ca60a9f3d1adb154170243.

--- prog_005.py
+++
@@ -1,8 +0,0 @@
-S1=input("Enter the string with combination of letters and numbers")
-
-count=0
-for i in S1:
-    if i.isdigit():
-        count+=1
-
-print(f"Total digits in given string is {count}")
